f59983ec8f8ee20991be5c050e87fd61d8dea26f
AledPeart <aledpeart@gmail.com>
2021-09-21 12:19:19 +0000

    Added find, edit and delete record functions, project completed

--- mongo_project.py
+++ mongo_project.py
@@ -25,6 +25,24 @@ def show_menu():
     option = input("Enter option: ")
     return option 
 
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+    except:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
  #these variables catch the user inputs
 def add_record():   
     print("")
@@ -54,6 +72,59 @@ def add_record():
     except: 
             print("Error accessing the database")
 
+        
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print (k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+                                
+                if update_doc[k] == "":
+                    update_doc[k] = v
+        
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print("")
+            print("Document updated")
+        except: 
+            print("Error accessing the database")
+
+
+def delete_record():
+     doc = get_record()
+     if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print (k.capitalize() + ": " + v.capitalize())
+
+        print("")
+        confirmation = input("Is this the documentyou want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == "y":
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Error accessing the database")
+        else:
+            print("Document not deleted")
+
+
+
 
 def main_loop():
     while True:
@@ -61,11 +132,11 @@ def main_loop():
         if option == "1":
             add_record()
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
